5eb0f74650f5b578702a37baeec769e7d3ca4b9d
Tanveer Singh <bhamratanveer00612@gmail.com>
2025-10-23 10:23:15 +0530

    Replace VideoHero component with ImageHero and update image source for checking performance in mobile

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,20 @@
-import Image from "next/image";
 import VideoHero from "@/components/VideoHero";
 import BackgroundBeamHome from "@/components/BackgroundBeamHome";
 import FeatureSection from "@/components/FeatureSection";
 import HomeCarousel from "@/components/HomeCarousel";
 import { BackgroundBoxes } from "@/components/BackgroundBoxes";
 import BackgroundImageReserve from "@/components/BackgroundImage";
+import ImageHero from "@/components/ImageHero";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-      <VideoHero
+      <ImageHero
           title="Welcome to NOC NOC"
           subtitle="Dubai's Ultimate Nightlife Destination"
-          videoUrl="https://res.cloudinary.com/dson4wzib/video/upload/v1761149130/get_gqchta.mp4"
+          imageUrl="nocnocImages/energyMeetsElegance.jpeg"
       />
-      <BackgroundBeamHome/>
+      {/* <BackgroundBeamHome/> */}
       <HomeCarousel/>
       <BackgroundBoxes />
       <FeatureSection/>
