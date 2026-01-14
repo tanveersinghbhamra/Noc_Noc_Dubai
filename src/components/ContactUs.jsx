c9d1185c4e62d4f1aeaf89eae31eadc73990654a
Tanveer Singh <bhamratanveer00612@gmail.com>
2026-01-14 16:57:13 +0530

    Update styling and layout in ContactUsSection for improved readability

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -86,23 +86,25 @@ export default function ContactUsSection() {
               </svg>
               <span>Timings 11:00 am - 2 am</span>
             </p>
-            <p className="flex items-center">
+            <p className="flex items-start">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="currentColor"
-                className="w-5 h-5 mr-2 sm:mr-6"
                 viewBox="0 0 20 20"
+                className="w-6 h-6 sm:w-5 sm:h-5 mr-3 sm:mr-6 flex-shrink-0"
               >
                 <path
                   fillRule="evenodd"
                   d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
                   clipRule="evenodd"
-                ></path>
+                />
               </svg>
-              <span>
-                   Noc Noc, Recreation Club, Near Ibn Battuta Mall, Mena Jabal Ali, Dubai, United Arab Emirates
+
+              <span className="text-sm sm:text-base">
+                Noc Noc, Recreation Club, Near Ibn Battuta Mall, Mena Jabal Ali, Dubai, United Arab Emirates
               </span>
             </p>
+
             <p className="flex items-center">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
