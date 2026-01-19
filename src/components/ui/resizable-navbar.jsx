c83200305e8ddfc487ab8def508e981e2c8a951d
Tanveer Singh <bhamratanveer00612@gmail.com>
2026-01-19 21:45:51 +0530

    Remove fixed positioning from Navbar component for improved layout flexibility

--- src/components/ui/resizable-navbar.jsx
+++ src/components/ui/resizable-navbar.jsx
@@ -37,7 +37,7 @@ export const Navbar = ({
     <motion.div
       ref={ref}
       // IMPORTANT: Change this to class of `fixed` if you want the navbar to be fixed
-      className={cn("fixed inset-x-0  z-40 w-full", className)}>
+      className={cn(" inset-x-0  z-40 w-full", className)}>
       {React.Children.map(children, (child) =>
         React.isValidElement(child)
           ? React.cloneElement(child, { visible })
